e380992230c735717b82b0699310fc669c9722f6
mimimomo <1249844726@qq.com>
2017-12-29 09:50:38 +0800

    ## 12.29

--- src/com/lexinsmart/action/JKLEDAction.java
+++ src/com/lexinsmart/action/JKLEDAction.java
@@ -1,51 +1,26 @@
 package com.lexinsmart.action;
 
-import java.util.Calendar;
-import java.util.Date;
-
 import com.lexinsmart.service.JKLEDService;
-import com.lexinsmart.utils.DateUtils;
 
 public class JKLEDAction {
 	public static void main(String args[]) {
-		/**   只用填写下面内容  从这里开始   **/
-		String ccpLastAccidentDate = "2017-12-10";// 确保格式，
-		String cbsLastAccidentDate = "2017-11-10";// 确保格式
-		String weatherType = "晴"; //天气
-		String fengxiang = "北风 3-4级"; //风力
-		String temperature = "气温 0-10度"; // 气温
-		String humidity = "湿度85%";//湿度
-		/**				到这里         **/
-		
-		
-		Date ccpDate = DateUtils.StringToDate(ccpLastAccidentDate);
-		Date cbsDate = DateUtils.StringToDate(cbsLastAccidentDate);
-		Date nowDate = new Date(System.currentTimeMillis());
-		int ccpLastAccidentDays = daysOfTwo(ccpDate, nowDate);
-		int cbsLastAccidentDays = daysOfTwo(cbsDate, nowDate);
-		//显示内容
-		String arg1 = "距" + ccpLastAccidentDate + "\n" 
-								+ "长春化工安全" + "\n" 
-								+ "生产无事故" + "\n" 
-								+ ccpLastAccidentDays + "天";
-		String arg2 = "距承包商安全"+"\n"
-								+ "无事故" + cbsLastAccidentDays+"天";
-		String arg3 = weatherType +"\n"
-								+ fengxiang +"\n"
-								+ temperature +"\n"
-								+humidity;
-		
-		JKLEDService jkledService = new JKLEDService(arg1, arg2, arg3);
-		jkledService.sendToLed();
-		
-	}
+		/** 只用填写下面内容 从这里开始 **/
+		String weatherType = "阵雨"; // 天气
+		String fengxiang = "东南风 3-4级"; // 风力
+		String temperature = "气温 8-12度"; // 气温
+		String humidity = "湿度79%";// 湿度
+		/** 到这里 **/
 
-	public static int daysOfTwo(Date fDate, Date oDate) {
-		Calendar aCalendar = Calendar.getInstance();
-		aCalendar.setTime(fDate);
-		int day1 = aCalendar.get(Calendar.DAY_OF_YEAR);
-		aCalendar.setTime(oDate);
-		int day2 = aCalendar.get(Calendar.DAY_OF_YEAR);
-		return day2 - day1;
+		/**
+		 * 将参数 拼接成 显示内容，控制显示内容的格式 
+		 * 大屏显示内容  1：距2015年7月7日 长春化工安全生产无事故  **天
+		 * 2：距承包商安全无事故**天
+		 * 3：天气信息：
+		 */
+		String weather = weatherType + "\n" + fengxiang + "\n" + temperature + "\n" + humidity;
+
+		JKLEDService jkledService = new JKLEDService(weather);
+		jkledService.sendToLed();
+
 	}
 }
--- src/com/lexinsmart/service/JKLEDService.java
+++ src/com/lexinsmart/service/JKLEDService.java
@@ -1,61 +1,42 @@
 package com.lexinsmart.service;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
-import org.apache.commons.httpclient.methods.multipart.Part;
-import org.apache.commons.httpclient.methods.multipart.StringPart;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 
 public class JKLEDService {
-	private String ccpDays;
-	private String cbsDays;
 	private String weather;
 
-	public JKLEDService(String ccpdays, String cbsdays, String weather) {
-		this.ccpDays = ccpdays;
-		this.cbsDays = cbsdays;
+	public JKLEDService( String weather) {
 		this.weather = weather;
 	}
 
-	public void sendToLed() {
+	/**
+	 * 发送数据给LED屏幕显示
+	 */
+	public void sendToLed(){
 //		 String url = "http://localhost:8080/JKLEDserver/jkl";
-		String url = "http://192.168.3.59:8038/server/ledscreen_weather";//ip地址
+		String url = "http://192.168.224.167:8038/server/ledscreen_weather";//ip地址
 
 		// =========定义HttpClient=========
 		HttpClient client = new HttpClient();
-	
 		// =========实例化HTTP方法 =========
 		PostMethod method = new PostMethod(url);
-		
 		method.setRequestHeader(new Header("Content-Type", "multipart/form-data"));
 		// 提交参数
-		 method.addParameter("ccpdays",ccpDays);
-		 method.addParameter("cbsdays",cbsDays);
 		 method.addParameter("weather",weather);
-		
-//		 NameValuePair name=new NameValuePair("ccpdays", "allen");
-//	        
-//	      NameValuePair[] data = {name}; 
-//	      method.setRequestBody(data);  
-	      
-	      
+     	      
 		method.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "UTF-8");
 		try {
 			int returnCode = client.executeMethod(method);
 			System.out.println("returnCode : " + returnCode);
-			System.out.println("============================================");
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 			System.out.println(method.getResponseBodyAsString());
-			System.out.println("======^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^=========");
+			System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 		} catch (HttpException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
