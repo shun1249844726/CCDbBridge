fde87ed5e98054ec7e9d74eef906731e03d3a50f
mimimomo <1249844726@qq.com>
2017-12-29 10:01:43 +0800

    ## 12.29
    Signed-off-by: mimimomo <1249844726@qq.com>

--- src/com/lexinsmart/action/JKLEDAction.java
+++ src/com/lexinsmart/action/JKLEDAction.java
@@ -5,7 +5,6 @@ import com.lexinsmart.service.JKLEDService;
 public class JKLEDAction {
 	public static void main(String args[]) {
 		/** 只用填写下面内容 从这里开始 **/
-
 		String weatherType = "阵雨"; // 天气
 		String fengxiang = "东南风 3-4级"; // 风力
 		String temperature = "气温 8-12度"; // 气温
@@ -15,9 +14,8 @@ public class JKLEDAction {
 
 		/**
 		 * 将参数 拼接成 显示内容，控制显示内容的格式 
-		 * 大屏显示内容  1：距2015年7月7日 长春化工安全生产无事故  **天
-		 * 2：距承包商安全无事故**天
-		 * 3：天气信息：
+		 * 大屏显示内容
+		 * 天气信息
 		 */
 		String weather = weatherType + "\n" + fengxiang + "\n" + temperature + "\n" + humidity;
 
