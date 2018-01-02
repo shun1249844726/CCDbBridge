7fd34e8eb8fe8a2312c782695a0909fa5867e0df
shun1249844726 <1249844726@qq.com>
2018-01-02 14:23:32 +0800

    只保留了天气的接口，其他删除了。

--- src/com/lexinsmart/action/JKLEDAction.java
+++ src/com/lexinsmart/action/JKLEDAction.java
@@ -24,6 +24,12 @@ public class JKLEDAction {
 		String weather = weatherType + "\n" + fengxiang + "\n" + temperature + "\n" + humidity;
 
 		JKLEDService jkledService = new JKLEDService(weather);
-		jkledService.sendToLed();
+		boolean isSuccessed = jkledService.sendToLed();
+		if (isSuccessed) {
+			System.out.println("发送成功显示成功");;
+		} else {
+			// 目标主机地址。192.168.224.167
+			System.out.println("发送成功显示失败，检查目标地址的防火墙是否关闭，网络是否通，能否ping通目标地址");;
+		}
 	}
 }
--- src/com/lexinsmart/service/JKLEDService.java
+++ src/com/lexinsmart/service/JKLEDService.java
@@ -18,12 +18,15 @@ public class JKLEDService {
 	/**
 	 * 发送数据给LED屏幕显示
 	 */
-	public void sendToLed(){
+	public boolean sendToLed(){
+		boolean isSuccessed = false;
 //		 String url = "http://localhost:8080/JKLEDserver/jkl";
 		String url = "http://192.168.224.167:8038/server/ledscreen_weather";//ip地址
 
 		// =========定义HttpClient=========
 		HttpClient client = new HttpClient();
+		client.getHttpConnectionManager().getParams().setConnectionTimeout(3000);  
+		client.getHttpConnectionManager().getParams().setSoTimeout(3000);  //等待结果超时
 		// =========实例化HTTP方法 =========
 		PostMethod method = new PostMethod(url);
 		method.setRequestHeader(new Header("Content-Type", "multipart/form-data"));
@@ -31,16 +34,19 @@ public class JKLEDService {
 		 method.addParameter("weather",weather);
      	      
 		method.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "UTF-8");
+
 		try {
 			int returnCode = client.executeMethod(method);
 			System.out.println("returnCode : " + returnCode);
-			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 			System.out.println(method.getResponseBodyAsString());
-			System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+			isSuccessed = true;
 		} catch (HttpException e) {
 			e.printStackTrace();
+			isSuccessed = false;
 		} catch (IOException e) {
 			e.printStackTrace();
+			isSuccessed = false;
 		}
+		return isSuccessed; 
 	}
 }
